9b86e3372eda2f6da675b23a7b0b164a1a77fec7
nxj1129 <niko.jugovac@gmail.com>
2021-04-26 18:27:44 +0200

    Connected commerce.js, added test data into the shop, finished navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import Products from "./components/products/Products";
 // import Navbar from "./components/navbar/Navbar";
-
 import { Products, Navbar } from "./components";
+import { commerce } from "./lib/commerce";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
   return (
     <div>
       <Navbar />
-      <Products />
+      <Products products={products} />
     </div>
   );
 }
